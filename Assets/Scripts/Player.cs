fdccc06fec615c6657b742e9cb97c44701bf0210
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-03-12 23:03:00 +1300

    added game session controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -72,6 +72,7 @@ public class Player : MonoBehaviour
             myAnimator.SetTrigger("Dying");
             GetComponent<Rigidbody2D>().velocity = deathKick;
             isAlive = false;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
         }
     }
 
